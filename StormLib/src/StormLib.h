052e4d45bcb906b8ffebe17c5069560c668ce600
H0zen <H0zen@users.noreply.github.com>
2016-08-30 00:59:31 +0300

    Forgotten files.

--- StormLib/src/StormLib.h
+++ StormLib/src/StormLib.h
@@ -98,7 +98,7 @@ extern "C" {
 //  Z - S for static-linked CRT library, D for multithreaded DLL CRT library
 //
 
-#if defined(_MSC_VER) && !defined(__STORMLIB_SELF__)
+/* #if defined(_MSC_VER) && !defined(__STORMLIB_SELF__)
   
   #ifdef _DEBUG                                 // DEBUG VERSIONS
     #ifndef _UNICODE                            
@@ -130,7 +130,7 @@ extern "C" {
     #endif
   #endif
 
-#endif
+#endif */
 
 //-----------------------------------------------------------------------------
 // Defines
