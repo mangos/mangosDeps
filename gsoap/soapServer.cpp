07a8ee2d1b9bbbf531cf1e716773eed9186188eb
Antz <billy1arm@users.noreply.github.com>
2021-01-31 20:35:55 +0000

    Update soapServer.cpp

--- gsoap/soapServer.cpp
+++ gsoap/soapServer.cpp
@@ -27,12 +27,16 @@ extern "C" SOAP_FMAC5 int SOAP_FMAC6 soap_serve(struct soap* soap)
     {
 #ifndef WITH_FASTCGI
         if (soap->keep_alive > 0 && soap->max_keep_alive > 0)
-            soap->keep_alive--;
+        {
+           soap->keep_alive--;
+        }
 #endif
         if (soap_begin_serve(soap))
         {
             if (soap->error >= SOAP_STOP)
-                continue;
+            {
+               continue;
+            }
             return soap->error;
         }
         if ((soap_serve_request(soap) || (soap->fserveloop && soap->fserveloop(soap))) && soap->error && soap->error < SOAP_STOP)
@@ -46,9 +50,11 @@ extern "C" SOAP_FMAC5 int SOAP_FMAC6 soap_serve(struct soap* soap)
 #ifdef WITH_FASTCGI
         soap_destroy(soap);
         soap_end(soap);
-    } while (1);
+    }
+    while (1);
 #else
-} while (soap->keep_alive);
+    } 
+    while (soap->keep_alive);
 #endif
 return SOAP_OK;
 }
@@ -58,7 +64,9 @@ extern "C" SOAP_FMAC5 int SOAP_FMAC6 soap_serve_request(struct soap* soap)
 {
     (void)soap_peek_element(soap);
     if (!soap_match_tag(soap, soap->tag, "ns1:executeCommand"))
-        return soap_serve_ns1__executeCommand(soap);
+    {
+       return soap_serve_ns1__executeCommand(soap);
+    }
     return soap->error = SOAP_NO_METHOD;
 }
 #endif
@@ -73,19 +81,28 @@ SOAP_FMAC5 int SOAP_FMAC6 soap_serve_ns1__executeCommand(struct soap* soap)
     soap_tmp_ns1__executeCommandResponse.result = &soap_tmp_string;
     soap_default_ns1__executeCommand(soap, &soap_tmp_ns1__executeCommand);
     if (!soap_get_ns1__executeCommand(soap, &soap_tmp_ns1__executeCommand, "ns1:executeCommand", NULL))
-        return soap->error;
+    {
+       return soap->error;
+    }
     if (soap_body_end_in(soap)
         || soap_envelope_end_in(soap)
         || soap_end_recv(soap))
+    {
         return soap->error;
+    }
     soap->error = ns1__executeCommand(soap, soap_tmp_ns1__executeCommand.command, soap_tmp_ns1__executeCommandResponse.result);
     if (soap->error)
+    {
         return soap->error;
+    }
     soap->encodingStyle = NULL; /* use SOAP literal style */
     soap_serializeheader(soap);
     soap_serialize_ns1__executeCommandResponse(soap, &soap_tmp_ns1__executeCommandResponse);
     if (soap_begin_count(soap))
+    {
         return soap->error;
+    }
+
     if ((soap->mode & SOAP_IO_LENGTH))
     {
         if (soap_envelope_begin_out(soap)
@@ -94,8 +111,11 @@ SOAP_FMAC5 int SOAP_FMAC6 soap_serve_ns1__executeCommand(struct soap* soap)
             || soap_put_ns1__executeCommandResponse(soap, &soap_tmp_ns1__executeCommandResponse, "ns1:executeCommandResponse", "")
             || soap_body_end_out(soap)
             || soap_envelope_end_out(soap))
+        {
             return soap->error;
+        }    
     };
+
     if (soap_end_count(soap)
         || soap_response(soap, SOAP_OK)
         || soap_envelope_begin_out(soap)
@@ -105,8 +125,9 @@ SOAP_FMAC5 int SOAP_FMAC6 soap_serve_ns1__executeCommand(struct soap* soap)
         || soap_body_end_out(soap)
         || soap_envelope_end_out(soap)
         || soap_end_send(soap))
+    {
         return soap->error;
-
+    }
     return soap_closesock(soap);
 }
 
